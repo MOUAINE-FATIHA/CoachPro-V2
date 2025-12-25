aae0008435b0a5d10f9e2bac5f69bbc1b53d9d9f
MOUAINE-FATIHA <fatihamouaine1@gmail.com>
2025-12-25 10:04:40 +0100

    suppression d'un fichier

--- README.md
+++
@@ -1,4 +0,0 @@
-# Plateforme de gestion de réservation sportif CoachPro
-
-## Description du projet
-Ce projet consiste à développer une plateforme web permettant de gérer les réservations sportif.
--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../classes/Utilisateur.php';
-// instanciation
-$user = new Utilisateur('a@gmail.com' , 'aaa');
-$user2 = new Utilisateur('BB@gmail.com' , 'BBB');
-var_dump($user,$user2);
-?>
